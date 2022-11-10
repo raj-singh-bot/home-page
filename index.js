ac3fc02fa4bb8469e13d52d7f826cc572b126204
OmBhingradiya <aaravpatel2189@gmail.com>
2022-11-11 00:00:03 +0530

    final

--- index.js
+++ index.js
@@ -28,3 +28,27 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+function scrollFunction() {
+  var menu = document.getElementById("myLinks");
+
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+  } else {
+    menu.style.display = "none";
+  }
+}
+
+window.onscroll = function () {
+  shrink();
+};
+
+function shrink() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").style.padding = "0px 0px";
+    // document.getElementById("hLogo").style.fontSize = "15px";
+  } else {
+    document.getElementById("navbar").style.padding = "10px 10px";
+    // document.getElementById("hLogo").style.fontSize = "25px";
+  }
+}
